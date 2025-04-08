2a4aa90a70471a33971b87fca7cc786490c0c81c
Tizazab <tizazabayana2@gmail.com>
2025-04-08 11:06:21 -0700

    Added delete and add to eemployee

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-employees',
@@ -21,7 +22,8 @@ import { MatSort, MatSortModule } from '@angular/material/sort';
     MatFormFieldModule,
     MatInputModule,
     MatSortModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSnackBarModule
   ],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css'
@@ -34,8 +36,12 @@ export class EmployeesComponent implements AfterViewInit {
   @ViewChild(MatSort) sort!: MatSort;
 
   isModalOpen = false;
+  departmentName: string = '';
+  departmentDescription: string = '';
+  isLoading = false;
+  
 
-  constructor(private employeesService: EmployeesService) {
+  constructor(private employeesService: EmployeesService , private snackBar: MatSnackBar) {
     this.dataSource = new MatTableDataSource<Employee>([]);
   }
 
@@ -77,9 +83,80 @@ export class EmployeesComponent implements AfterViewInit {
     this.isModalOpen = false;
   }
 
-  addEmployee() {
-    console.log('Employee Added');
-    this.closeModal();
-    this.loadEmployees(); // Refresh the table after adding
-  }
+ // Add department
+   addDepartment() {
+     console.log('Department Added');
+     console.log('Name:', this.departmentName);
+     console.log('Description:', this.departmentDescription);
+ 
+     const newDepartment: Partial<Employee> = {
+       firstName: this.departmentName,
+       lastName: this.departmentDescription,
+       email: this.departmentDescription,
+       phone: this.departmentDescription,
+       address: this.departmentDescription,
+       birthDate: this.departmentDescription,
+       hireDate: this.departmentDescription,
+     };
+ 
+     this.isLoading = true;
+ 
+     this.employeesService.addEmployee(newDepartment).subscribe({
+       next: (response) => {
+         console.log('Department Added Successfully:', response.employee);
+         this.snackBar.open('Department deleted successfully!', 'Close', {
+           duration: 3000,
+           horizontalPosition: 'center',  
+           verticalPosition: 'top', 
+           panelClass: ['snack-bar-success']
+         });
+         this.closeModal();
+         this.loadEmployees(); 
+       },
+       error: (error) => {
+         console.error('Error adding department:', error);
+         this.snackBar.open('Error adding department' + error, 'Close', {
+           duration: 3000,
+           horizontalPosition: 'center',  
+           verticalPosition: 'top', 
+          });
+         this.isLoading = false;
+       },
+       complete: () => {
+         this.isLoading = false;
+       },
+     });
+   }
+ 
+   // Delete department
+   deleteDepartment(departmentId: number) {
+     console.log('Department Deleted');
+     if (confirm('Are you sure you want to delete this department?')) {
+       this.isLoading = true;
+       this.employeesService.deleteEmployee(departmentId).subscribe({
+         next: (response) => {
+           console.log('Department Deleted Successfully:', response.message);
+           this.snackBar.open('Department deleted successfully!', 'Close', {
+             duration: 3000,
+             horizontalPosition: 'center',  
+             verticalPosition: 'top', 
+             panelClass: ['snack-bar-success']      
+           });
+           this.loadEmployees(); 
+         },
+         error: (error) => {
+           console.error('Error deleting department:', error);
+           this.snackBar.open('Error deleting department'+ error, 'Close', {
+             duration: 3000,
+             horizontalPosition: 'center',  
+             verticalPosition: 'top', 
+            });
+           this.isLoading = false;
+         },
+         complete: () => {
+           this.isLoading = false;
+         },
+       });
+     }
+   }
 }
